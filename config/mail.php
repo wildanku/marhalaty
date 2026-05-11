72c3ffd68979a31f77d46e8c9ddb51ca0a54c509
wildanmaulana <wildaanmaulana@gmail.com>
2026-05-11 10:23:38 +0800

    feat(mail): add sync mailer configuration for SMTP transport

--- config/mail.php
+++ config/mail.php
@@ -79,6 +79,17 @@ return [
             'transport' => 'array',
         ],
 
+        'sync' => [
+            'transport' => 'smtp',
+            'scheme' => env('MAIL_SCHEME'),
+            'host' => env('MAIL_HOST', '127.0.0.1'),
+            'port' => env('MAIL_PORT', 2525),
+            'username' => env('MAIL_USERNAME'),
+            'password' => env('MAIL_PASSWORD'),
+            'timeout' => null,
+            'local_domain' => env('MAIL_EHLO_DOMAIN', parse_url((string) env('APP_URL', 'http://localhost'), PHP_URL_HOST)),
+        ],
+
         'failover' => [
             'transport' => 'failover',
             'mailers' => [
